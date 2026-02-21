1a516651e887e9cdb6ac62e8f11517cbe05ff8f9
Muhamad Romdon <muhamad.romdon@ilcs.co.id>
2026-02-21 15:20:29 +0700

    fix profile page type error

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -574,7 +574,7 @@ export default function Profile() {
           </Card>
         </motion.div>
 
-        {userHouse && (
+        {userHouse?.houses && (
           <motion.div
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
